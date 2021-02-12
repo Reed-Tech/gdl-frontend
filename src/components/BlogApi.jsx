93d696e69746643d370242adca9d97514d2ab753
Ememobong-dev <Ememobongumoren@gmail.com>
2021-02-12 13:47:33 +0100

    updae again

--- src/components/BlogApi.jsx
+++ src/components/BlogApi.jsx
@@ -1,28 +1,28 @@
-import React, { useContext } from "react";
-import { BlogContext } from "../components/BlogContext";
-// import BlogPost from "../components/BlogPost";
+// import React, { useContext } from "react";
+// import { BlogContext } from "../components/BlogContext";
+// // import BlogPost from "../components/BlogPost";
 
-function BlogApi() {
-  const { data } = useContext(BlogContext);
-  console.log(data);
+// function BlogApi() {
+//   const { data } = useContext(BlogContext);
+//   console.log(data);
 
-  return (
-    <div>
-      <h1>Hello</h1>
-      {/* <h1 className="head__text"></h1> */}
-      {/* <div className="all__post"> */}
-        {/* {data
-          ? "HIiii"
-          : "Loading"}
-          {data ? console.log(data.title) : "Loading Blog"} */}
-            {/* {data
-          ? data.posts.map((post) => (
-            <BlogPost data={post} key={post.id} />
-          ))
-          : "Loading"} */}
-      {/* </div> */}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       {/* <h1 className="head__text"></h1> */}
+//       {/* <div className="all__post"> */}
+//         {/* {data
+//           ? "HIiii"
+//           : "Loading"}
+//           {data ? console.log(data.title) : "Loading Blog"} */}
+//             {/* {data
+//           ? data.posts.map((post) => (
+//             <BlogPost data={post} key={post.id} />
+//           ))
+//           : "Loading"} */}
+//       {/* </div> */}
+//     </div>
+//   );
+// }
 
-export default BlogApi;
+// export default BlogApi;
