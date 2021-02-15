9c4340df46d333b144ca4330319c140cf502240d
Ememobong-dev <Ememobongumoren@gmail.com>
2021-02-15 18:25:14 +0100

    update on what we do larger screen

--- src/components/HeaderSocialLinks.jsx
+++ src/components/HeaderSocialLinks.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import YoutubeIcon from '../assets/images/Youtube.svg'
+// import YoutubeIcon from '../assets/images/Youtube.svg'
 import FacebookIcon from '../assets/images/Facebook.svg'
 import TwitterIcon from '../assets/images/Twitter.svg'
+import {FiInstagram} from 'react-icons/fi/index.esm'
 
 
 const HeaderSocialLinks = (props) => {
@@ -9,7 +10,8 @@ const HeaderSocialLinks = (props) => {
             <ul className={props.customClass? props.customClass : 'HeaderSocialLinks'}>
                 <li><a href="facebook"><img src={FacebookIcon} alt="Facebook Link"/></a></li>
                 <li><a href="twitter"><img src={TwitterIcon} alt="Twitter Link"/></a></li>
-                <li><a href="youtube"><img src={YoutubeIcon} alt="Youtube Link"/></a></li>
+                {/* <li><a href="youtube"><img src={YoutubeIcon} alt="Youtube Link"/></a></li> */}
+                <li><a href="instagram"><FiInstagram  size={'20px'} color={"white"} /> </a></li>
             </ul>
   
     )
