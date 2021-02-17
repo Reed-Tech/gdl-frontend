0c8b41e7887d5ead2be84f6de7532e8fb8d282c6
Ememobong-dev <Ememobongumoren@gmail.com>
2021-02-17 01:20:24 +0100

    ...

--- src/components/BlogSlider.jsx
+++ src/components/BlogSlider.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import BlogCard from './BlogCard';
-
 
 const BlogSlider = (props) => {
     console.log(props);
