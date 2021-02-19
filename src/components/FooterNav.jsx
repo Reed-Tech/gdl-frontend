a68feeaca2b6bde0f193fdeeb852edb5203f5723
Ememobong-dev <Ememobongumoren@gmail.com>
2021-02-19 18:30:20 +0100

    content update

--- src/components/FooterNav.jsx
+++ src/components/FooterNav.jsx
@@ -8,32 +8,6 @@ const FooterNav = (props) => {
     const goToHome = () => window.scrollTo(0, 0)
 
     return (
-        // <div className='footerNav'>
-        //     <div className='col-sm-12 col-lg-4 w-100 footerNav_services'>
-        //        <p>Our Services</p>
-        //        <ul>
-        //            <li>Invest Now</li>
-        //            <li>Money Market Fund</li>
-        //            <li>Buy Shares</li>
-        //            <li>Asset Management</li>
-        //            <li>Finance and leashing</li>
-        //            <li>Research</li>
-        //        </ul>
-        //     </div>
-        //     <nav className='col-sm-12 col-md-6 col-lg-4 w-100 d-flex justify-content-between flex-wrap footerNav_links align-content-center'>
-        //         <NavLink to='/#home' onClick={goToHome}>Home</NavLink>
-        //         <NavLink to='/blog' onClick={goToHome}>Blog</NavLink>
-        //         <NavLink to='/media' onClick={goToHome}>Media</NavLink>
-        //         <NavLink to='/career' onClick={goToHome}>Career</NavLink>
-        //     </nav>
-        //     <div className='col-sm-12 col-lg-4 w-100 d-flex'>
-        //         <HeaderSocialLinks customClass='footerNav_socialLinks' />
-        //     </div>
-        //     <div className="col-sm-12 col-md-12 col-lg-4 w-100 d-flex justify-content-between flex-wrap footerNav_copyright">
-        //         <p>Copyright © {myDate.getFullYear()} GDL. All Rights Reserved.</p>
-        //     </div>
-
-        // </div>
         <div className="footerNav py-5">
             <div className="container">
                 <div className="row mt-5">
