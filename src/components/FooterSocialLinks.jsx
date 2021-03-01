eb3ea2019a5e6145170d5ef33eaeb924dc3b72f6
Ememobong-dev <Ememobongumoren@gmail.com>
2021-03-01 01:14:35 +0100

    icons

--- src/components/FooterSocialLinks.jsx
+++ src/components/FooterSocialLinks.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import YoutubeIcon from '../assets/images/Youtube.svg'
 import FacebookIcon from '../assets/images/Facebook.svg'
 import TwitterIcon from '../assets/images/Twitter.svg'
-import InstagramIcon from '../assets/images/instagram (2).svg'
-// import EmailIcon from '../assets/images/email (1).svg'
-import LinkedInIcon from '../assets/images/linkedin.svg'
-import PhoneIcon from '../assets/images/phone-call (1).svg'
+import { FaInbox, FaInstagram, FaLinkedin,  FaPhoneAlt } from 'react-icons/fa'
 
 
 
@@ -14,11 +11,12 @@ const FooterSocialLinks = (props) => {
             <ul className={props.customClass? props.customClass : 'HeaderSocialLinks'}>
                 <li><a href="https://www.facebook.com/GDLAssetManagement"><img src={FacebookIcon} alt="Facebook Link"/></a></li>
                 <li><a href="https://twitter.com/gdl_nigeria?s=08"><img src={TwitterIcon} alt="Twitter Link"/></a></li>
-                <li><a href="youtube"><img src={YoutubeIcon} alt="Youtube Link"/></a></li>
-                <li><a href="https://www.instagram.com/gdl_nigeria/?igshid=db676hd70sc4"><img src={InstagramIcon} alt="Instagram Link"/></a></li>
-                {/* <li><a href="email"><img src={EmailIcon} alt="Email Link"/></a></li> */}
-                <li><a href="https://www.linkedin.com/company/gdl-nigeria/"><img src={LinkedInIcon} alt="Linkedin Link"/></a></li>
-                <li><a href="tel:+234 (0) 705 443 5000"><img src={PhoneIcon} alt="Phone No"/></a></li>
+                <li><a href="https://www.youtube.com/channel/UC8hKfXLTtoyqHhnL1qEwNBQ"><img src={YoutubeIcon} alt="Youtube Link"/></a></li>
+                <li><a href="https://www.instagram.com/gdl_nigeria/?igshid=db676hd70sc4"> <FaInstagram size="20px"  /> </a></li>
+                <li><a href="mailto:customerservice@gdl.com.ng"> <FaInbox size="20px" /> </a></li>
+                <li><a href="https://www.linkedin.com/company/gdl-nigeria/"> <FaLinkedin size="20px" /> </a></li>
+                <li><a href="tel:+234 (0) 705 443 5000"> <FaPhoneAlt size="20px" /> </a></li>
+                
             </ul>
   
     )
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,35 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import { Modal } from "react-bootstrap";
+import NewsletterBg from "../assets/images/newsletter.png"
 
-function PopUp
-() {
+const PopUp = () =>{
     
-        const [show, setShow] = useState(true);
-        const handleClose = () => setShow(false);
+    const [show, setShow] = useState(true);
+    const handleClose = () => setShow(false);
 
-        useEffect(() => {
-            const popupModalValue = localStorage.getItem("popupModal")
-            if (!popupModalValue) {
-              const timer = setTimeout(() => {
-                setShow(true);
-                localStorage.setItem("popupModal", "1");
-              }, 2000);
-          
-              return () => clearTimeout(timer);
-            }
-          }, []);
+    useEffect(() => {
+        const popupModalValue = localStorage.getItem("popupModal")
+        if (!popupModalValue) {
+          const timer = setTimeout(() => {
+            setShow(true);
+            localStorage.setItem("popupModal", "1");
+          }, 2000);
+      
+          return () => clearTimeout(timer);
+        }
+      }, []);
 
     return (
-        <Modal 
+          <Modal 
             show={show} 
             onHide={handleClose} 
-            centered>
+            backdrop="static"
+            // keyboard={false}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            >
             <Modal.Header closeButton>
-                <Modal.Title>
-                    Learn React
-                </Modal.Title>
+              <div className="login_box_area section_gap">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <div className="login_box_img">
+                        <img className="img-fluid" src={NewsletterBg} alt="newsletter Bg" />
+                      </div>
+                    </div>
+                    <div className="col-lg-6">
+                      <div className="login_form_inner">
+                        <form className="row login_form" >
+                          <p>Subscribe to our newsletter and be the first to receive updates on our new investment opportunities and promos.</p>
+                          <div className="col-md-12 form-group">
+                            <input type="email" className="form-control" id="email" name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" />
+                          </div>
+                          <div className="col-md-12 form-group">
+                            <button type="submit" value="submit" className="primary-btn">Log In</button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+       
             </Modal.Header>
-        </Modal>
+         
+          </Modal>
     )
 }
 
